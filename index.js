e29a0669823008d45b198c4984651779639905ed
matthew rendely <matthew.rendely@gmail.com>
2023-03-18 12:05:45 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email){
+  return fetch('http://localhost:3000/users',{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(
+      {name: name,
+      email: email}
+    )
+  })
+  .then(r => r.json())
+  .then(d => {
+    console.log("new id:",d.id);
+    document.body.innerHTML = `${d.id}`;
+  })
+  .catch(e => {
+    console.log(e)
+    document.body.innerHTML = `${e}`;
+  })
+}
